61146095dd504a6b4090d3699d8896bcf2c79b91
John Ernsthausen <John@JohnErnsthausen.com>
2016-04-08 12:47:05 -0400

    P1

--- rpolytest.cpp
+++ rpolytest.cpp
@@ -1,19 +1,31 @@
 #include "gmock/gmock.h"
 
+#include "rpoly.h"
+
 using namespace testing;
 
 // START:RPolyStub
-class RPolyStub {
+class RPolyStub: public RPoly {
   public:
     int maxDegree;
     int mdp1;
     RPolyStub(int degree);
+    ~RPolyStub(void);
+
+    void initialize() override;
+    void rpoly(double* op, int Degree, double* zeror, double* zeroi) override;
 };
 
-RPolyStub::RPolyStub(int degree) {
+RPolyStub::RPolyStub(int degree): RPoly(degree) {
   maxDegree = degree;
   mdp1 = degree+1;
 }
+
+RPolyStub::~RPolyStub(void) {}
+
+void RPolyStub::initialize() {}
+
+void RPolyStub::rpoly(double* op, int Degree, double* zeror, double* zeroi) {}
 // END:RPolyStub
 
 TEST(RpolyInterface, ConstructorPopulatesMaximalDegree) {
@@ -26,7 +38,13 @@ TEST(RpolyInterface, ConstructorPopulatesMaximalDegreePlusOne) {
   ASSERT_THAT(stub.mdp1, Eq(11));
 }
 
-TEST(RpolyInterface, HasBaseClass) {
+TEST(RpolyInterface, HasBaseClassVariableMaxDegree) {
   RPoly* rpoly{nullptr};
+  rpoly = new RPolyStub(10);
+
+  ASSERT_THAT(rpoly->maxDegree, Eq(10));
+  
+  delete rpoly;
+  rpoly = nullptr;
 }
 
