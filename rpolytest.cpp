dd42c08ab7c0b1a1c187ea3567791644b2ba464d
John Ernsthausen <John@JohnErnsthausen.com>
2016-04-08 11:55:19 -0400

    P2

--- rpolytest.cpp
+++ rpolytest.cpp
@@ -1,13 +1,17 @@
-#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using namespace testing;
 
 // START:RPolyStub
 class RPolyStub {
   public:
+    int maxDegree;
     RPolyStub(int degree);
 };
 
 RPolyStub::RPolyStub(int degree) {
-};
+  maxDegree = degree;
+}
 // END:RPolyStub
 
 TEST(RpolyInterface, ConstructorPopulatesMaximalDegree) {
