7d1455b197d9374e7ccb07d8737d06dd6207f286
John Ernsthausen <John@JohnErnsthausen.com>
2016-04-08 14:07:15 -0400

    Refactored RPolyStub to its own header class
    Refactored rpolytest.cpp to use a factory

---
+++ rpolystub.h
@@ -0,0 +1,60 @@
+#ifndef RPolyStub_h
+#define RPolyStub_h
+
+#include <stdexcept>
+
+class RPolyStub: public RPoly {
+  public:
+    int maxDegree;
+    int mdp1;
+    RPolyStub(int degree);
+    ~RPolyStub(void);
+
+    void initialize() override;
+    void rpoly(double* op, int Degree, double* zeror, double* zeroi) override;
+};
+
+RPolyStub::RPolyStub(int degree): RPoly(degree) {
+  maxDegree = degree;
+  mdp1 = degree+1;
+}
+
+RPolyStub::~RPolyStub(void) {}
+
+void RPolyStub::initialize() {}
+
+void RPolyStub::rpoly(double* op, int Degree, double* zeror, double* zeroi) {
+  if (Degree > maxDegree){
+    throw std::invalid_argument( "Requested maximal degree is greater than MAXDEGREE." );
+  }
+
+ if (op[0]==0.0) {
+    throw std::invalid_argument( "The leading coefficient is zero." );
+  }
+
+  if (Degree == 9) {
+    zeror[0] = 0.065297428539351;
+    zeror[1] =-1.0;
+    zeror[2] =-2.0;
+    zeror[3] =-3.0;
+    zeror[4] =-4.0;
+    zeror[5] =-0.07;
+    zeror[6] = 1.0;
+    zeror[7] = 2.0;
+    zeror[8] = 3.0;
+    zeror[9] = 4.0;
+    zeroi[0] = 0.0;
+    zeroi[1] = 0.0;
+    zeroi[2] = 0.0;
+    zeroi[3] =-2.0;
+    zeroi[4] =-2.0;
+    zeroi[5] = 0.0;
+    zeroi[6] = 0.0;
+    zeroi[7] = 0.0;
+    zeroi[8] = 2.0;
+    zeroi[9] = 2.0;
+  }
+}
+
+#endif
+
--- rpolytest.cpp
+++ rpolytest.cpp
@@ -1,85 +1,57 @@
 #include "gmock/gmock.h"
 
 #include "rpoly.h"
-#include <stdexcept>
+#include "rpolystub.h"
 
 using namespace testing;
 
 #define DEGREE 10
 
-// START:RPolyStub
-class RPolyStub: public RPoly {
+class RpolyInterface: public Test {
   public:
-    int maxDegree;
-    int mdp1;
-    RPolyStub(int degree);
-    ~RPolyStub(void);
+    RPoly* rpoly{nullptr};
+    double* op{nullptr};
+    double* zr{nullptr};
+    double* zi{nullptr};
 
-    void initialize() override;
-    void rpoly(double* op, int Degree, double* zeror, double* zeroi) override;
+    void SetUp() override {
+      rpoly = new RPolyStub(DEGREE);
+      op = new double[DEGREE+1];
+      zr = new double[DEGREE];
+      zi = new double[DEGREE];
+    }
+
+    void TearDown() override {
+      delete [] op;
+      delete [] zr;
+      delete [] zi;
+      op = nullptr;
+      zr = nullptr;
+      zi = nullptr;
+      delete rpoly;
+      rpoly = nullptr;
+    }
 };
 
-RPolyStub::RPolyStub(int degree): RPoly(degree) {
-  maxDegree = degree;
-  mdp1 = degree+1;
-}
-
-RPolyStub::~RPolyStub(void) {}
-
-void RPolyStub::initialize() {}
-
-void RPolyStub::rpoly(double* op, int Degree, double* zeror, double* zeroi) {
-  if (Degree > maxDegree){
-    throw std::invalid_argument( "Requested maximal degree is greater than MAXDEGREE." );
-  }
-
- if (op[0]==0.0) {
-    throw std::invalid_argument( "The leading coefficient is zero." );
-  }
-}
-// END:RPolyStub
-
-TEST(RpolyInterface, ConstructorPopulatesMaximalDegree) {
+TEST_F(RpolyInterface, ConstructorPopulatesMaximalDegree) {
   RPolyStub stub(DEGREE);
   ASSERT_THAT(stub.maxDegree, Eq(DEGREE));
 }
 
-TEST(RpolyInterface, ConstructorPopulatesMaximalDegreePlusOne) {
+TEST_F(RpolyInterface, ConstructorPopulatesMaximalDegreePlusOne) {
   RPolyStub stub(DEGREE);
   ASSERT_THAT(stub.mdp1, Eq(DEGREE+1));
 }
 
-TEST(RpolyInterface, HasBaseClassVariableMaxDegree) {
-  RPoly* rpoly{nullptr};
-  rpoly = new RPolyStub(DEGREE);
-
+TEST_F(RpolyInterface, HasBaseClassVariableMaxDegree) {
   ASSERT_THAT(rpoly->maxDegree, Eq(DEGREE));
-  
-  delete rpoly;
-  rpoly = nullptr;
 }
 
-TEST(RpolyInterface, HasBaseClassVariableMaxDegreePlusOne) {
-  RPoly* rpoly{nullptr};
-  rpoly = new RPolyStub(DEGREE);
-
+TEST_F(RpolyInterface, HasBaseClassVariableMaxDegreePlusOne) {
   ASSERT_THAT(rpoly->mdp1, Eq(DEGREE+1));
-  
-  delete rpoly;
-  rpoly = nullptr;
 }
 
-TEST(RpolyInterface, ExceptionThrownForLeadingCoefficientOfZero) {
-  RPoly* rpoly{nullptr};
-  rpoly = new RPolyStub(DEGREE);
-
-  double* op{nullptr};
-  double* zr{nullptr};
-  double* zi{nullptr};
-  op = new double[DEGREE+1];
-  zr = new double[DEGREE];
-  zi = new double[DEGREE];
-
+TEST_F(RpolyInterface, ExceptionThrownForLeadingCoefficientOfZero) {
   try {
     op[0] = 0.0;
     rpoly->rpoly(op,DEGREE,zr,zi);
@@ -88,28 +60,9 @@ TEST(RpolyInterface, ExceptionThrownForLeadingCoefficientOfZero) {
   catch (const std::invalid_argument& expected) {
     ASSERT_STREQ(expected.what(),"The leading coefficient is zero.");
   }
-
-  delete [] op;
-  delete [] zr;
-  delete [] zi;
-  delete rpoly;
-  op = nullptr;
-  zr = nullptr;
-  zi = nullptr;
-  rpoly = nullptr;
 }
 
-TEST(RpolyInterface, ExceptionThrownForExceedingMaximalDegree) {
-  RPoly* rpoly{nullptr};
-  rpoly = new RPolyStub(DEGREE);
-
-  double* op{nullptr};
-  double* zr{nullptr};
-  double* zi{nullptr};
-  op = new double[DEGREE+1];
-  zr = new double[DEGREE];
-  zi = new double[DEGREE];
-
+TEST_F(RpolyInterface, ExceptionThrownForExceedingMaximalDegree) {
   try {
     rpoly->rpoly(op,DEGREE+1,zr,zi);
     FAIL() << "Expected std::invalid_argument";
@@ -117,14 +70,5 @@ TEST(RpolyInterface, ExceptionThrownForExceedingMaximalDegree) {
   catch (const std::invalid_argument& expected) {
     ASSERT_STREQ(expected.what(),"Requested maximal degree is greater than MAXDEGREE.");
   }
-
-  delete [] op;
-  delete [] zr;
-  delete [] zi;
-  delete rpoly;
-  op = nullptr;
-  zr = nullptr;
-  zi = nullptr;
-  rpoly = nullptr;
 }
 
