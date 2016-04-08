f1983aee9a0483273af1fb3cdecd80038a7f330b
John Ernsthausen <John@JohnErnsthausen.com>
2016-04-08 15:49:15 -0400

    P2

--- rootstest.cpp
+++ rootstest.cpp
@@ -1,10 +1,41 @@
 #include "gmock/gmock.h"
 
+#include "rpoly.h"
+#include "rpolystub.h"
+
+using namespace testing;
+
 class Roots {
+  int maxDegree;
+
+  public:
+    Roots(RPoly* rpoly);
+    ~Roots(void);
+    int getMaxDegree(void) const;
+
+  private:
+    RPoly* rpoly_;
 };
 
-TEST(RootFinder, GetMaximalDegree) {
-  Roots rootfinder();
+Roots::Roots(RPoly* rpoly) : rpoly_(rpoly) {
+  maxDegree = rpoly_->maxDegree;
+}
+
+Roots::~Roots(void) {
+}
+
+int Roots::getMaxDegree(void) const {
+  return maxDegree;
+}
+
+TEST(RootFinder, GetMaximalDegreeFromStub) {
+  RPoly* rpoly{nullptr};
+  rpoly = new RPolyStub(10);
+
+  Roots rootfinder(rpoly);
   ASSERT_THAT(rootfinder.getMaxDegree(), Eq(10));
+  
+  delete rpoly;
+  rpoly = nullptr;
 }
 
