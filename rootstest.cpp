e4d73b05dd34de325615da457ffcb7e0f25e00ff
John Ernsthausen <John@JohnErnsthausen.com>
2016-04-08 16:45:13 -0400

    P1

--- rootstest.cpp
+++ rootstest.cpp
@@ -10,26 +10,65 @@ using namespace testing;
 class Roots {
   int maxDegree;
   int mdp1;
+  int degree;
 
   public:
     Roots(RPoly* rpoly);
     ~Roots(void);
     int getMaxDegree(void) const;
+    void findRoots(const std::vector<double>& coeff);
+    void getRoots(int Degree, std::vector<double>& zr, std::vector<double>& zi) const;
 
   private:
     RPoly* rpoly_;
+
+    double* zeror{nullptr};
+    double* zeroi{nullptr};
+    double* op{nullptr};
 };
 
 Roots::Roots(RPoly* rpoly) : rpoly_(rpoly) {
   maxDegree = rpoly_->maxDegree;
   mdp1  = rpoly_->mdp1;
+  zeror = new double[maxDegree];
+  zeroi = new double[maxDegree];
+  op    = new double[mdp1];
 }
 
 Roots::~Roots(void) {
+  delete [] zeror;
+  delete [] zeroi;
+  delete [] op;
+  zeror = nullptr;
+  zeroi = nullptr;
+  op    = nullptr;
+  rpoly_= nullptr;
 }
 
 int Roots::getMaxDegree(void) const {
   return maxDegree;
+}
+
+void Roots::findRoots(const std::vector<double>& coeff) {
+  degree = coeff.size()-1;
+  for(int j=0; j<=degree; j++) {
+    op[j] = coeff[j];
+  }
+
+  rpoly_->initialize();
+  // Know length(coeff) .leq. length(op) because degree .leq. rpoly_->maxDegree
+  rpoly_->rpoly(op, degree, zeror, zeroi);
+
+  return;
+}
+
+void Roots::getRoots(int Degree, std::vector<double>& zr, std::vector<double>& zi) const {
+  Degree = degree;
+  for(int j=0; j<=degree; j++) {
+    zr.push_back(zeror[j]);
+    zi.push_back(zeroi[j]);
+  }
+  return;
 }
 
 TEST(RootFinder, GetMaximalDegreeFromStub) {
@@ -51,9 +90,8 @@ TEST(RootFinder, FindRootsSpecifiedInStubForDegreeNine) {
   std::vector<double> coeff = {1,2,3,4,5,6,7,8,9,10};
   rootfinder.findRoots(coeff);
   
-  int degree;
-  size_t size = coeff.size();
-  std::vector<double> zr[size], zi[size];
+  int degree{0};
+  std::vector<double> zr, zi;
   rootfinder.getRoots(degree, zr, zi);
 
   EXPECT_THAT(zr[0], Eq( 0.065297428539351));
